6ec86b453613685290dbba55ac31415732fcfa01
Christina Kozanian <christinakozanian@gmail.com>
2023-11-09 23:05:48 -0800

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+import { fetch } from "whatwg-fetch";
+
+function App() {
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [dog, setDog] = useState(null)
+
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r => r.json())
+        .then((data) => {
+            setDog(data)
+            setIsLoaded(true)
+        })
+
+    },[])
+
+    if (!isLoaded){
+        return <p>Loading...</p>
+    }
+
+    return (
+    <div>
+        <img src={dog.message} alt='A Random Dog'></img>
+    </div>
+)}
+
+export default App;
